307b3e252062246bed99df0cc024065da5c2a6f8
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-03 19:56:41 -0300

    Add new script

---
+++ scripts/dmenu/reset-wallpaper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/Downloads/Wallpapers/
+feh --bg-scale Landscape\ V.jpg
+notify-send -i /usr/share/icons/Adwaita/symbolic/emotes/face-smirk-symbolic.svg -t 2500 "Wallpaper reset"
+
