b8818088f4c4acd88817cc505943f9aa9b8e6306
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-05 09:13:47 -0300

    Add vim multiline plugin

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -57,6 +57,11 @@ local plugins = {
     end,
   },
 
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
