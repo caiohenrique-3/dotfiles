6838bb584e29bf222e494f1d17b26ff3c22623ac
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-02-01 23:26:05 -0300

    add new scripts & update some

---
+++ scripts/dmenu/print-to-clipboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+maim -s | xclip -selection clipboard -t image/png
+notify-send -i /usr/share/icons/Adwaita/symbolic/legacy/applets-screenshooter-symbolic.svg -t 2500 "Screenshot copied to clipboard"
