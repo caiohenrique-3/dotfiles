a393306f82030730612297c65525704915647f69
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-10-06 20:02:12 -0300

    improvements

--- scripts/dmenu/reset-wallpaper.sh
+++ scripts/dmenu/reset-wallpaper.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Read the current wallpaper from .fehbg
+current_wallpaper=$(grep -v "feh --bg-fill ~/Pictures/black.jpg" ~/.fehbg)
+
+# Check if the current wallpaper is not the placeholder
+if [[ "$current_wallpaper" != *"black.jpg"* ]]; then
+  echo "$current_wallpaper" > ~/.last_wallpaper
+fi
+
+# Reset the wallpaper to the placeholder
 feh --bg-fill ~/Pictures/black.jpg
+
+# Send a notification
 notify-send -t 2500 "Wallpaper reset  ( ͡° ͜ʖ ͡°) "
