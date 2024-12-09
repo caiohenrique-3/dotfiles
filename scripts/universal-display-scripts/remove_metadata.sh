2ecdbfbc145367fe90ee9abc58860303ee7ac6b7
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-12-09 07:58:03 -0300

    improve remove_metadata script

--- scripts/universal-display-scripts/remove_metadata.sh
+++ scripts/universal-display-scripts/remove_metadata.sh
@@ -5,6 +5,33 @@ if [ "$#" -eq 0 ]; then
   exit 1
 fi
 
+generate_random_date() {
+  year=$((RANDOM % 13 + 2009))
+
+  month=$((RANDOM % 12 + 1))
+
+  if [ "$month" -eq 2 ]; then
+    if [ $((year % 4)) -eq 0 ] && { [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; }; then
+      # Leap year
+      day=$((RANDOM % 29 + 1))
+    else
+      # Non-leap year
+      day=$((RANDOM % 28 + 1))
+    fi
+  elif [ "$month" -eq 4 ] || [ "$month" -eq 6 ] || [ "$month" -eq 9 ] || [ "$month" -eq 11 ]; then
+    day=$((RANDOM % 30 + 1))
+  else
+    day=$((RANDOM % 31 + 1))
+  fi
+
+  printf "%04d-%02d-%02d" "$year" "$month" "$day"
+}
+
+file_count="$#"
+echo "Removing metadata of $file_count files..."
+
+index=1
+
 for file in "$@"; do
   if [ ! -f "$file" ]; then
     echo "File not found: $file"
@@ -13,7 +40,7 @@ for file in "$@"; do
 
   exiftool -overwrite_original -All= "$file"
 
-  fixed_date="2012-01-01"
+  fixed_date=$(generate_random_date)
 
   random_hour=$(printf "%02d" $((RANDOM % 24)))
   random_minute=$(printf "%02d" $((RANDOM % 60)))
@@ -23,5 +50,9 @@ for file in "$@"; do
 
   touch -a -m -d "$random_time" "$file"
 
-  echo "Metadata removed and last modification and access time set for $file: $random_time"
+  echo "$index: $file set to $random_time"
+
+  index=$((index + 1))
 done
+
+echo "All files processed successfully."
