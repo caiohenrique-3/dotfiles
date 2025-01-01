3818a7accfc49a7807806f0c3cbcda95ed325e88
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2025-01-01 11:31:11 -0300

    improve formatting, add new env variables, fix native games script

--- scripts/universal-display-scripts/bubblejail/example_native.sh
+++ scripts/universal-display-scripts/bubblejail/example_native.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 GAME_EXEC="/path/to/game/executable"
-WINE_PREFIX="/path/to/prefix"
-WINE_CMD="/path/to/wine/binary"
+GAME_DIR=$(dirname "$GAME_EXEC")
+MANGOHUD_CONFIGFILE="/path/to/config.conf"
 
-bubblejail run native_games sh -c "WINEPREFIX=$WINE_PREFIX $WINE_CMD $GAME_EXEC"
+bubblejail run native_games sh -c "
+    cd $GAME_DIR &&
+    MANGOHUD_CONFIGFILE=$MANGOHUD_CONFIGFILE \
+    mangohud $GAME_EXEC
+"
--- scripts/universal-display-scripts/bubblejail/example_wine.sh
+++ scripts/universal-display-scripts/bubblejail/example_wine.sh
@@ -2,6 +2,13 @@
 
 GAME_EXEC="/path/to/game/executable"
 WINE_PREFIX="/path/to/prefix"
-WINE_CMD="/path/to/wine/binary"
+WINE_CMD="/path/to/wine/executable"
+MANGOHUD_CONFIGFILE="/path/to/config.conf"
 
-bubblejail run wine_games sh -c "WINEPREFIX=$WINE_PREFIX $WINE_CMD $GAME_EXEC"
+bubblejail run wine_games sh -c "
+    WINEPREFIX=$WINE_PREFIX \
+    WINEFSYNC=1 \
+    env -u DISPLAY WAYLAND_DISPLAY=wayland-0 \
+    MANGOHUD_CONFIGFILE=$MANGOHUD_CONFIG \
+    mangohud $WINE_CMD $GAME_EXEC
+"
